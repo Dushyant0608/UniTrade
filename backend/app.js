56a10cd3823882ede6770398067bcf9f6cfa70f2
Dushyant <dushyantyaduvanshi06@gmail.com>
2026-03-19 12:44:39 +0530

    feat: add error handling middleware and async handler utility; update dependencies

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const app = express();
+const cookie = require("cookie-parser");
+
+const AppError =  require("./src/utils/appError");
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookie());
+
+//Global Error Handler
+app.use((err, req , res , next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.isOperational ? err.message : "Something went wrong";
+
+    res.status(statusCode).json({
+        success : false,
+        message
+    });
+});
+
 
 app.get("/" , (req,res)=>{
     res.send("server is running");
